5d5f310ff5b2d0b733b67d2a5ce965050c6bb156
sagar8958 <85739150+sagar8958@users.noreply.github.com>
2021-06-14 16:29:26 +0530

    Add files via upload

---
+++ Java/sagar pokhriyal/MyProfile.java
@@ -0,0 +1,7 @@
+public class MyProfile
+{
+    public static void main(String args[]) {
+        System.out.println("Name: Sagar");
+        System.out.println("Roll No: 1922053");
+    }
+}
---
+++ Java/sagar pokhriyal/UC.java
@@ -0,0 +1,9 @@
+class UC {
+    public static void main(String args[])
+    {
+        String str = "my name is sagar";
+  
+        String UC = str.toUpperCase();
+        System.out.println(UC);
+    }
+}
